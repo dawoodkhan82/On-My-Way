f31a96384c8da7b4008a5ccfc7463b7389804efb
dawoodkhan82 <dawoodkhan82@gmail.com>
2017-05-08 16:18:28 -0400

    Swift 3 updates 1.2

--- ios_yelp_swift-master/Yelp/BusinessesViewController.swift
+++ ios_yelp_swift-master/Yelp/BusinessesViewController.swift
@@ -15,14 +15,18 @@ class BusinessesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
             self.businesses = businesses
-        
-            for business in businesses {
-                print(business.name!)
-                print(business.address!)
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
             }
-        })
+            
+        }
+        )
 
 /* Example of Yelp search with more search options specified
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
